31f0ada98e1c95fc17edda321ec81ee75b487af1
lzj <toolgood@qq.com>
2019-07-05 10:34:13 +0800

    修改扩展方法

--- ToolGood.Bedrock.Web/Theme/ReflectionExtensions.cs
+++ ToolGood.Bedrock.Web/Theme/ReflectionExtensions.cs
@@ -5,38 +5,38 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 
-namespace System
+namespace ToolGood.Bedrock.Web.Theme
 {
-    public static class ReflectionExtensions
+    internal static class ReflectionExtensions
     {
-        public static bool HasProperty<T>(this T obj, string name)
-            where T : class
-        {
-            return obj.GetType().GetRuntimeProperty(name) != null;
-        }
+        //public static bool HasProperty<T>(this T obj, string name)
+        //    where T : class
+        //{
+        //    return obj.GetType().GetRuntimeProperty(name) != null;
+        //}
 
-        public static bool HasValue<T>(this T obj, string name)
-             where T : class
-        {
-            var currentProperty = obj.GetType().GetRuntimeProperty(name);
-            if (currentProperty == null)
-                return false;
-            var caurrentValue = currentProperty.GetValue(obj);
-            var defaultValue = Activator.CreateInstance(obj.GetType()).GetType().GetRuntimeProperty(name).GetValue(obj);
-            return caurrentValue != defaultValue;
-        }
+        //public static bool HasValue<T>(this T obj, string name)
+        //     where T : class
+        //{
+        //    var currentProperty = obj.GetType().GetRuntimeProperty(name);
+        //    if (currentProperty == null)
+        //        return false;
+        //    var caurrentValue = currentProperty.GetValue(obj);
+        //    var defaultValue = Activator.CreateInstance(obj.GetType()).GetType().GetRuntimeProperty(name).GetValue(obj);
+        //    return caurrentValue != defaultValue;
+        //}
 
-        public static void SetValue<T>(this T obj, string name, object value)
-             where T : class
-        {
-            obj.GetType().GetRuntimeProperty(name).SetValue(obj, value);
-        }
+        //public static void SetValue<T>(this T obj, string name, object value)
+        //     where T : class
+        //{
+        //    obj.GetType().GetRuntimeProperty(name).SetValue(obj, value);
+        //}
 
-        public static object GetValue<T>(this T obj, string name)
-         where T : class
-        {
-            return obj.GetType().GetRuntimeProperty(name).GetValue(obj);
-        }
+        //public static object GetValue<T>(this T obj, string name)
+        // where T : class
+        //{
+        //    return obj.GetType().GetRuntimeProperty(name).GetValue(obj);
+        //}
 
 
         public static IEnumerable<T> GetImplementationsOf<T>(this Assembly assembly)
