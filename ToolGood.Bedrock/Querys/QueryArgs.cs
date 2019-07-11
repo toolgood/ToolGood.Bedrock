de640d6a0eeea9d95724cb0f35c024839fba9142
lzj <toolgood@qq.com>
2019-07-11 08:52:30 +0800

    清理代码

--- ToolGood.Bedrock/Querys/QueryArgs.cs
+++ ToolGood.Bedrock/Querys/QueryArgs.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Threading;
 using ToolGood.AntiDuplication;
 using ToolGood.Bedrock.Internals;
+using Newtonsoft.Json;
 
 namespace ToolGood.Bedrock
 {
@@ -12,8 +13,6 @@ namespace ToolGood.Bedrock
     /// </summary>
     public class QueryArgs
     {
-        //private static AntiDupQueue<string, CancellationTokenSource> antiDupQueue = new AntiDupQueue<string, CancellationTokenSource>(10000);
-
         public QueryArgs()
         {
             SqlTimes = new List<DebugSqlTime>();
@@ -22,6 +21,7 @@ namespace ToolGood.Bedrock
 
 
         internal HttpContext HttpContext;
+
         public HttpContext GetHttpContext() { return HttpContext; }
 
         public void SetHttpContext(HttpContext value) { HttpContext = value; }
@@ -31,7 +31,6 @@ namespace ToolGood.Bedrock
         /// </summary>
         public bool? UseLog { get; set; }
 
-        #region 日期 时间 
         private DateTime? _UseToday;
 
         /// <summary>
@@ -61,158 +60,25 @@ namespace ToolGood.Bedrock
             get { return _UseNow; }
         }
 
-        #endregion
-
         /// <summary>
         /// 批序号
         /// </summary>
         public string BatchNum { get; set; }
 
-        //private CancellationTokenSource CancellationTokenSource;
-        //public CancellationTokenSource GetCancellationTokenSource()
-        //{
-        //    if (CancellationTokenSource != null) {
-        //        return CancellationTokenSource;
-        //    }
-
-        //    if (string.IsNullOrWhiteSpace(BatchNum)) {
-        //        return null;
-        //    }
-        //    if (antiDupQueue.TryGetValue(BatchNum, out CancellationTokenSource)) {
-        //        return CancellationTokenSource;
-        //    }
-        //    return null;
-        //}
-        //public void SetCancellationTokenSource(CancellationTokenSource value)
-        //{
-        //    if (value != null) {
-        //        CancellationTokenSource = value;
-        //        if (string.IsNullOrWhiteSpace(BatchNum) == false) {
-        //            antiDupQueue.TryAdd(BatchNum, value);
-        //        }
-        //    }
-        //}
-
 
         /// <summary>
         /// sql执行时间
         /// </summary>
+        [JsonIgnore]
         public List<DebugSqlTime> SqlTimes { get; set; }
 
         /// <summary>
         /// log记录
         /// </summary>
+        [JsonIgnore]
         public List<DebugLog> Logs { get; set; }
 
-        //#region DateTime function
-
-        ///// <summary>
-        ///// 获取当前时间
-        ///// </summary>
-        ///// <param name="isRealNow">是否取真实的电脑时间</param>
-        ///// <returns></returns>
-        //public DateTime GetNow(bool isRealNow = false)
-        //{
-        //    if (UseLog == false) { return DateTime.Now; }
-
-        //    if (isRealNow) {
-        //        return DateTime.Now;
-        //    }
-        //    if (UseNow != null && UseNow.HasValue) {
-        //        return UseNow.Value;
-        //    }
-        //    if (UseToday != null && UseToday.HasValue) {
-        //        var now = DateTime.Now;
-        //        var date = UseToday.Value.Date;
-        //        return new DateTime(date.Year, date.Month, date.Day, now.Hour, now.Minute, now.Second);
-        //    }
-        //    return DateTime.Now;
-        //}
-
-        ///// <summary>
-        ///// 获取当前时间
-        ///// </summary>
-        ///// <param name="isRealNow">是否取真实的电脑时间</param>
-        ///// <returns></returns>
-        //public DateTime GetUtcNow(bool isRealNow = false)
-        //{
-        //    if (UseLog == false) { return DateTime.UtcNow; }
-
-        //    if (isRealNow) {
-        //        return DateTime.UtcNow;
-        //    }
-        //    if (UseNow != null && UseNow.HasValue) {
-        //        var date = UseToday.Value.Date;
-        //        return new DateTime(date.Year, date.Month, date.Day, date.Hour, date.Minute, date.Second, DateTimeKind.Utc);
-        //    }
-        //    if (UseToday != null && UseToday.HasValue) {
-        //        var now = DateTime.Now;
-        //        var date = UseToday.Value.Date;
-        //        return new DateTime(date.Year, date.Month, date.Day, now.Hour, now.Minute, now.Second, DateTimeKind.Utc);
-        //    }
-        //    return DateTime.UtcNow;
-        //}
-
-        ///// <summary>
-        ///// 获取真实的Now
-        ///// </summary>
-        ///// <returns></returns>
-        //public DateTime GetRealNow()
-        //{
-        //    return DateTime.Now;
-        //}
-
-        ///// <summary>
-        ///// 获取真实的UtcNow
-        ///// </summary>
-        ///// <returns></returns>
-        //public DateTime GetRealUtcNow()
-        //{
-        //    return DateTime.UtcNow;
-        //}
-
-
-        ///// <summary>
-        ///// 获取当前时间的时间戳
-        ///// </summary>
-        ///// <param name="isRealNow">是否取真实的电脑时间</param>
-        ///// <param name="isUtcNow">是否为UtcNow</param>
-        ///// <returns></returns>
-        //public long GetTimestamp(bool isRealNow = false, bool isUtcNow = false)
-        //{
-        //    var now = isUtcNow ? GetUtcNow(isRealNow) : GetNow(isRealNow);
-        //    return (long)(now - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Local)).TotalSeconds;
-        //}
-
-        ///// <summary>
-        ///// 将时间戳转为时间
-        ///// </summary>
-        ///// <param name="totalSeconds">秒</param>
-        ///// <param name="kind"></param>
-        ///// <returns></returns>
-        //public DateTime ToDateTime(long totalSeconds, DateTimeKind kind = DateTimeKind.Local)
-        //{
-        //    var start = new DateTime(1970, 1, 1, 0, 0, 0, kind);
-        //    return start.AddSeconds(totalSeconds);
-        //}
-
-        //#endregion
-
-        //public void CopyTo(QueryArgs inArgs)
-        //{
-        //    inArgs.CancellationTokenSource = CancellationTokenSource;//必须放在前面
-
-        //    inArgs.UseNow = UseNow;
-        //    inArgs.UseToday = UseToday;
-
-        //    inArgs.BatchNum = BatchNum;
-        //    inArgs.UseLog = UseLog;
-
-        //    inArgs.SqlTimes = SqlTimes;
-        //    inArgs.Logs = Logs;
-
-        //    inArgs.SetHttpContext(HttpContext);
-        //}
+         
 
     }
 
--- ToolGood.Bedrock/Querys/QueryResult.cs
+++ ToolGood.Bedrock/Querys/QueryResult.cs
@@ -10,15 +10,24 @@ namespace ToolGood.Bedrock
     /// </summary>
     public class QueryResult
     {
+        /// <summary>
+        /// code码
+        /// </summary>
         [JsonProperty("code",NullValueHandling = NullValueHandling.Ignore)]
         public int Code { get; set; }
-
+        /// <summary>
+        /// code描术
+        /// </summary>
         [JsonProperty("state", NullValueHandling = NullValueHandling.Ignore)]
         public string State { get; set; }
-
+        /// <summary>
+        /// 消息
+        /// </summary>
         [JsonProperty("message", NullValueHandling = NullValueHandling.Ignore)]
         public string Message { get; set; }
-
+        /// <summary>
+        /// 
+        /// </summary>
         [JsonProperty("data", NullValueHandling = NullValueHandling.Ignore)]
         public object Data { get; set; }
 
@@ -27,16 +36,24 @@ namespace ToolGood.Bedrock
         /// </summary>
         [JsonProperty("ciphertext", NullValueHandling = NullValueHandling.Ignore)]
         public string Ciphertext { get; set; }
-
+        /// <summary>
+        /// 扩展数据
+        /// </summary>
         [JsonProperty("extendData", NullValueHandling = NullValueHandling.Ignore)]
         public object ExtendData { get; set; }
-
+        /// <summary>
+        /// sql执行日志
+        /// </summary>
         [JsonProperty("sqlTimes", NullValueHandling = NullValueHandling.Ignore)]
         public List<DebugSqlTime> SqlTimes { get; set; }
-
+        /// <summary>
+        /// 日志
+        /// </summary>
         [JsonProperty("logs", NullValueHandling = NullValueHandling.Ignore)]
         public List<DebugLog> Logs { get; set; }
-
+        /// <summary>
+        /// 页面
+        /// </summary>
         [JsonProperty("page", NullValueHandling = NullValueHandling.Ignore)]
         public Page Page { get; set; }
 
