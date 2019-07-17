da772875585afc871c87b1b2ff02169827a40f57
toolgood <toolgood@qq.com>
2019-07-17 08:03:20 +0800

    去除无效代码

--- ToolGood.Bedrock.Web/Controllers/ViewComponentBase.cs
+++ ToolGood.Bedrock.Web/Controllers/ViewComponentBase.cs
@@ -1,30 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
-using ToolGood.Bedrock.Web.Constants;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using System.Threading.Tasks;
+//using ToolGood.Bedrock.Web.Constants;
 
-namespace ToolGood.Bedrock.Web
-{
-    [ViewComponent]
-    public abstract class ViewComponentBase : ViewComponent
-    {
-        protected int SuccessCode { get { return CommonConstants.SuccessCode; } }
-        protected int ErrorCode { get { return CommonConstants.ErrorCode; } }
-        protected QueryArgs QueryArgs { get; set; }
+//namespace ToolGood.Bedrock.Web
+//{
+//    [ViewComponent]
+//    public abstract class ViewComponentBase : ViewComponent
+//    {
+//        protected int SuccessCode { get { return CommonConstants.SuccessCode; } }
+//        protected int ErrorCode { get { return CommonConstants.ErrorCode; } }
+//        protected QueryArgs QueryArgs { get; set; }
 
-        public Task<IViewComponentResult> InvokeAsync()
-        {
-            ViewData["SuccessCode"] = SuccessCode;
-            ViewData["ErrorCode"] = ErrorCode;
+//        public Task<IViewComponentResult> InvokeAsync()
+//        {
+//            ViewData["SuccessCode"] = SuccessCode;
+//            ViewData["ErrorCode"] = ErrorCode;
 
-            if (HttpContext.Items.ContainsKey("ToolGood.Bedrock.QueryArgsBase")) {
-                QueryArgs = HttpContext.Items["ToolGood.Bedrock.QueryArgsBase"] as QueryArgs;
-                LogUtil.QueryArgs = QueryArgs;
-                ViewData["QueryArgs"] = QueryArgs;
-            }
-            return InvokeView();
-        }
-        protected abstract Task<IViewComponentResult> InvokeView();
+//            if (HttpContext.Items.ContainsKey("ToolGood.Bedrock.QueryArgsBase")) {
+//                QueryArgs = HttpContext.Items["ToolGood.Bedrock.QueryArgsBase"] as QueryArgs;
+//                LogUtil.QueryArgs = QueryArgs;
+//                ViewData["QueryArgs"] = QueryArgs;
+//            }
+//            return InvokeView();
+//        }
+//        protected abstract Task<IViewComponentResult> InvokeView();
 
 
-    }
-}
+//    }
+//}
