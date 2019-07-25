cf45c694d083b68dbc0321aa2b34abb796765110
lzj <toolgood@qq.com>
2019-07-25 14:14:19 +0800

    修改字典扩展

--- ToolGood.Bedrock/Extensions/DictionaryExtension.cs
+++ ToolGood.Bedrock/Extensions/DictionaryExtension.cs
@@ -7,11 +7,11 @@ namespace System
     public static partial class ObjectExtension
     {
         /// <summary>
-        /// 尝试更新
+        /// 尝试更新，注：字典为空，默认new 一个字典对象
         /// </summary>
         /// <typeparam name="T1"></typeparam>
         /// <typeparam name="T2"></typeparam>
-        /// <param name="data"></param>
+        /// <param name="data">为空时，默认new 一个字典对象</param>
         /// <param name="key"></param>
         /// <param name="value"></param>
         /// <returns></returns>
@@ -27,65 +27,82 @@ namespace System
         }
 
         /// <summary>
-        /// 设置键-值,更新或添加
+        /// 尝试更新，注：字典为空，默认new 一个字典对象
         /// </summary>
         /// <typeparam name="T1"></typeparam>
         /// <typeparam name="T2"></typeparam>
-        /// <param name="data"></param>
+        /// <param name="data">为空时，默认new 一个字典对象</param>
         /// <param name="key"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        public static Dictionary<T1, T2> SetValue<T1, T2>(this Dictionary<T1, T2> data, T1 key, T2 value)
+        public static Dictionary<T1, T2> TryUpdateOnly<T1, T2>(this Dictionary<T1, T2> data, T1 key, Func<T2> value)
         {
             if (data == null) { data = new Dictionary<T1, T2>(); }
             if (null != key) {
                 if (data.ContainsKey(key)) {
-                    data[key] = value;
-                } else {
-                    data.Add(key, value);
+                    data[key] = value();
                 }
             }
             return data;
         }
 
         /// <summary>
-        /// 根据键移除
-        /// </summary>
-        /// <typeparam name="T1"></typeparam>
-        /// <typeparam name="T2"></typeparam>
-        /// <param name="data"></param>
-        /// <param name="key">键值</param>
-        /// <returns></returns>
-        public static Dictionary<T1, T2> DeleteKey<T1, T2>(this Dictionary<T1, T2> data, T1 key)
-        {
-            if (data == null) return data;
-            if (data.ContainsKey(key)) {
-                data.Remove(key);
-            }
-            return data;
-        }
-
-
-        /// <summary>
-        /// 批量添加
+        /// 批量添加，注：字典为空，默认new 一个字典对象
         /// </summary>
         /// <typeparam name="T1"></typeparam>
         /// <typeparam name="T2"></typeparam>
         /// <typeparam name="T3"></typeparam>
-        /// <param name="data"></param>
+        /// <param name="data">为空时，默认new 一个字典对象</param>
         /// <param name="list"></param>
         /// <param name="keyFun"></param>
         /// <param name="valueFun"></param>
         /// <returns></returns>
-        public static Dictionary<T1, T2> AddRange<T1, T2, T3>(this Dictionary<T1, T2> data, IEnumerable<T3> list, Func<T3, T1> keyFun, Func<T3, T2> valueFun)
+        public static IDictionary<T1, T2> AddRange<T1, T2, T3>(this IDictionary<T1, T2> data, IEnumerable<T3> list, Func<T3, T1> keyFun, Func<T3, T2> valueFun)
         {
-            if (data == null) return data;
-            //var keyFun = key.Compile();
-            //var valueFun = value.Compile();
+            if (data == null) { data = new Dictionary<T1, T2>(); }
+
             foreach (var item in list) {
                 data[keyFun(item)] = valueFun(item);
             }
             return data;
+        }
+
+        /// <summary>
+        /// 尝试获取，不存在则添加，注：字典为空，默认new 一个字典对象
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="T2"></typeparam>
+        /// <param name="data">为空时，默认new 一个字典对象</param>
+        /// <param name="key"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static T2 TryGetOrAdd<T1, T2>(this Dictionary<T1, T2> data, T1 key, Func<T2> func)
+        {
+            if (data == null) { data = new Dictionary<T1, T2>(); }
+
+            if (data.TryGetValue(key, out T2 t2)) { return t2; }
+            var val = func();
+            data[key] = val;
+            return val;
+        }
+
+
+        /// <summary>
+        /// 尝试获取，不存在则添加，注：字典为空，默认new 一个字典对象
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="T2"></typeparam>
+        /// <param name="data">为空时，默认new 一个字典对象</param>
+        /// <param name="key"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static T2 TryGetOrAdd<T1, T2>(this Dictionary<T1, T2> data, T1 key, T2 defaultValue)
+        {
+            if (data == null) { data = new Dictionary<T1, T2>(); }
+
+            if (data.TryGetValue(key, out T2 t2)) { return t2; }
+            data[key] = defaultValue;
+            return defaultValue;
         }
 
 
