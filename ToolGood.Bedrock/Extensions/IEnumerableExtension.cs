5384158ae0c2e52120503d6d371b24d90d80876d
lzj <toolgood@qq.com>
2019-08-02 11:07:43 +0800

    添加扩展方法

--- ToolGood.Bedrock/Extensions/IEnumerableExtension.cs
+++ ToolGood.Bedrock/Extensions/IEnumerableExtension.cs
@@ -186,6 +186,8 @@ namespace System
                 : source;
         }
 
+
+
         /// <summary>
         /// 忽略大小写的字符串相等比较，判断是否以任意一个待比较字符串相等
         /// </summary>
@@ -230,6 +232,33 @@ namespace System
         }
 
 
+        /// <summary>忽略大小写的字符串开始比较，判断是否以任意一个待比较字符串开始</summary>
+        /// <param name="value">字符串</param>
+        /// <param name="strs">待比较字符串数组</param>
+        /// <returns></returns>
+        public static Boolean StartsWith(this IEnumerable<string> strs, string value)
+        {
+            if (String.IsNullOrEmpty(value)) return false;
+
+            foreach (var item in strs) {
+                if (value.StartsWith(item)) return true;
+            }
+            return false;
+        }
+
+        /// <summary>忽略大小写的字符串结束比较，判断是否以任意一个待比较字符串结束</summary>
+        /// <param name="value">字符串</param>
+        /// <param name="strs">待比较字符串数组</param>
+        /// <returns></returns>
+        public static Boolean EndsWith(this IEnumerable<string> strs, string value)
+        {
+            if (String.IsNullOrEmpty(value)) return false;
+
+            foreach (var item in strs) {
+                if (value.EndsWith(item)) return true;
+            }
+            return false;
+        }
     }
 
 }
--- ToolGood.Bedrock/Extensions/StringExtension.cs
+++ ToolGood.Bedrock/Extensions/StringExtension.cs
@@ -11,23 +11,13 @@ namespace System
     public static partial class ObjectExtension
     {
         #region 字符串扩展
-        /// <summary>忽略大小写的字符串相等比较，判断是否以任意一个待比较字符串相等</summary>
-        /// <param name="value">字符串</param>
-        /// <param name="strs">待比较字符串数组</param>
-        /// <returns></returns>
-        public static Boolean EqualIgnoreCase(this String value, params String[] strs)
-        {
-            foreach (var item in strs) {
-                if (String.Equals(value, item, StringComparison.OrdinalIgnoreCase)) return true;
-            }
-            return false;
-        }
+
 
         /// <summary>忽略大小写的字符串开始比较，判断是否以任意一个待比较字符串开始</summary>
         /// <param name="value">字符串</param>
         /// <param name="strs">待比较字符串数组</param>
         /// <returns></returns>
-        public static Boolean StartsWithIgnoreCase(this String value, params String[] strs)
+        public static Boolean IsStartsInWithIgnoreCase(this String value, params String[] strs)
         {
             if (String.IsNullOrEmpty(value)) return false;
 
@@ -41,7 +31,7 @@ namespace System
         /// <param name="value">字符串</param>
         /// <param name="strs">待比较字符串数组</param>
         /// <returns></returns>
-        public static Boolean EndsWithIgnoreCase(this String value, params String[] strs)
+        public static Boolean IsEndsInWithIgnoreCase(this String value, params String[] strs)
         {
             if (String.IsNullOrEmpty(value)) return false;
 
@@ -57,7 +47,7 @@ namespace System
         /// <param name="value"></param>
         /// <param name="strs"></param>
         /// <returns></returns>
-        public static bool ContainsIgnoreCase(this string value, params string[] strs)
+        public static bool IsContainsInWithIgnoreCase(this string value, params string[] strs)
         {
             if (String.IsNullOrEmpty(value)) return false;
             var lowStr = value.ToLowerInvariant();
@@ -69,24 +59,12 @@ namespace System
             return false;
         }
 
-
-        /// <summary>忽略大小写的字符串相等比较，判断是否以任意一个待比较字符串相等</summary>
-        /// <param name="value">字符串</param>
-        /// <param name="strs">待比较字符串数组</param>
-        /// <returns></returns>
-        public static Boolean EqualIgnoreCase(this String value, IEnumerable<string> strs)
-        {
-            foreach (var item in strs) {
-                if (String.Equals(value, item, StringComparison.OrdinalIgnoreCase)) return true;
-            }
-            return false;
-        }
-
+ 
         /// <summary>忽略大小写的字符串开始比较，判断是否以任意一个待比较字符串开始</summary>
         /// <param name="value">字符串</param>
         /// <param name="strs">待比较字符串数组</param>
         /// <returns></returns>
-        public static Boolean StartsWithIgnoreCase(this String value, IEnumerable<string> strs)
+        public static Boolean IsStartsInWithIgnoreCase(this String value, IEnumerable<string> strs)
         {
             if (String.IsNullOrEmpty(value)) return false;
 
@@ -100,7 +78,7 @@ namespace System
         /// <param name="value">字符串</param>
         /// <param name="strs">待比较字符串数组</param>
         /// <returns></returns>
-        public static Boolean EndsWithIgnoreCase(this String value, IEnumerable<string> strs)
+        public static Boolean IsEndsInWithIgnoreCase(this String value, IEnumerable<string> strs)
         {
             if (String.IsNullOrEmpty(value)) return false;
 
@@ -116,7 +94,7 @@ namespace System
         /// <param name="value"></param>
         /// <param name="strs"></param>
         /// <returns></returns>
-        public static bool ContainsIgnoreCase(this string value, IEnumerable<string> strs)
+        public static bool IsContainsInWithIgnoreCase(this string value, IEnumerable<string> strs)
         {
             if (String.IsNullOrEmpty(value)) return false;
             var lowStr = value.ToLowerInvariant();
