a9f23b301b429676097d8aea45ba9a9b06845a23
lzj <toolgood@qq.com>
2019-10-17 11:39:30 +0800

    修改rxtaskutil

--- ToolGood.Bedrock/Utils/RxTaskUtil.cs
+++ ToolGood.Bedrock/Utils/RxTaskUtil.cs
@@ -1,31 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reactive.Subjects;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using ToolGood.Bedrock.Utils.TaskSchedulers;
 
 namespace ToolGood.Bedrock.Utils
 {
     /// <summary>
-    /// 
+    /// 仿 Rx.NET 订阅 分发
     /// </summary>
-    public class RxTaskUtil : IDisposable
+    public static class RxTaskUtil
     {
-        private static RxTaskUtil Instance = new RxTaskUtil();
-        private List<Subject<SubjectObject>> Subjects = new List<Subject<SubjectObject>>();
+        private static ConcurrentBag<Action<SubjectObject>> Subscribes = new ConcurrentBag<Action<SubjectObject>>();
+        private static TaskFactory factory;
+        private static int _ThreadCount;
+
+        /// <summary>
+        /// 线程数
+        /// </summary>
+        public static int ThreadCount {
+            get { return _ThreadCount; }
+            set {
+                _ThreadCount = value;
+                factory = new TaskFactory(new LimitedConcurrencyLevelTaskScheduler(_ThreadCount));
+            }
+        }
+
+        static RxTaskUtil()
+        {
+            ThreadCount = Environment.ProcessorCount;
+            factory = new TaskFactory(new LimitedConcurrencyLevelTaskScheduler(ThreadCount));
+        }
+
+        /// <summary>
+        /// 订阅
+        /// </summary>
+        /// <param name="action"></param>
+        public static void Subscribe(Action<string> action)
+        {
+            Subscribes.Add((p) => {
+                try {
+                    action(p.Args);
+                } catch { }
+            });
+        }
 
         /// <summary>
         /// 订阅
         /// </summary>
         /// <param name="key"></param>
         /// <param name="action"></param>
-        public static void Subscribe(string key, Action<object[]> action)
+        public static void Subscribe(string key, Action<string> action)
         {
-            var subject = new Subject<SubjectObject>();
-            subject.Subscribe(p => {
+            if (string.IsNullOrEmpty(key)) {
+                Subscribe(action);
+                return;
+            }
+            Subscribes.Add((p) => {
                 if (p.Key == key) {
-                    action(p.Args);
+                    try {
+                        action(p.Args);
+                    } catch { }
                 }
             });
-            Instance.Subjects.Add(subject);
         }
 
         /// <summary>
@@ -36,29 +73,29 @@ namespace ToolGood.Bedrock.Utils
         public static void RunTask(string key, params object[] args)
         {
             SubjectObject subjectObject = new SubjectObject(key, args);
-            foreach (var subject in Instance.Subjects) {
-                subject.OnNext(subjectObject);
+            var _factory = factory;
+            foreach (var subject in Subscribes) {
+                _factory.StartNew(() => {
+                    subject(subjectObject);
+                });
             }
         }
-
-        public void Dispose()
-        {
-            foreach (var inputs in Instance.Subjects) {
-                inputs.Dispose();
-            }
-            Subjects.Clear();
-        }
-
 
         class SubjectObject
         {
             public SubjectObject(string key, object[] args)
             {
                 Key = key;
-                Args = args;
+                if (args != null) {
+                    if (args.Length == 1) {
+                        Args = args[0].ToJson();
+                    } else {
+                        Args = args.ToJson();
+                    }
+                }
             }
             public string Key { get; private set; }
-            public object[] Args { get; private set; }
+            public string Args { get; private set; }
         }
     }
 }
